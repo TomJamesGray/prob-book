09395fc24f8f8d9ef15daeec0fc8cfd19cdd7429
Tom Gray <tomg77@yahoo.co.uk>
2019-08-13 21:19:28 +0100

    got basic functions working with (possibly dodgy) arg unpacking

--- prob_book/parsing/evaluation.py
+++ prob_book/parsing/evaluation.py
@@ -1,7 +1,37 @@
+import math
 from lark import Transformer
 from lark import v_args
+
+funcs = {
+    "sin":{
+        "n": 1,
+        "func": lambda x: math.sin(x),
+        "level": 5,
+        "regex_name": "sin"
+    },
+    "cos": {
+        "n": 1,
+        "func": lambda x: math.cos(x),
+        "level": 5,
+        "regex_name": "cos"
+    },
+    "tan": {
+        "n": 1,
+        "func": lambda x: math.tan(x),
+        "level": 5,
+        "regex_name": "tan"}
+}
 
 @v_args(inline=True)
 class EvalLine(Transformer):
     from operator import add,sub,mul,truediv as div,neg
     number = float
+
+    def func_call(self,name,*args):
+        f = funcs[name]["func"]
+        print(args)
+        unpacked = []
+        for val in args[0].children:
+            unpacked.append(val.children[0])
+        print(unpacked)
+        return f(*unpacked)
