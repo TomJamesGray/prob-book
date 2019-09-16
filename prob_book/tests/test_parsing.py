3910cba2b5a68f244b9d43e499716202170fd756
Tom Gray <tomg77@yahoo.co.uk>
2019-09-16 16:35:31 +0100

    Fixed unit tests

--- prob_book/tests/test_parsing.py
+++ prob_book/tests/test_parsing.py
@@ -4,5 +4,5 @@ from prob_book.parsing import parser
 def test_define_dist():
     # Check parsing functions define a distribution
     main.defined_vars = {}
-    parser.parse("X~Po(1)")
+    parser.Parser().parse("X~Po(1)")
     assert "X" in main.defined_vars.keys()
--- prob_book/tests/test_prob.py
+++ prob_book/tests/test_prob.py
@@ -3,10 +3,10 @@ from prob_book.parsing import parser
 
 def test_prob_po_eq():
     main.defined_vars = {}
-    parser.parse("X~Po(1)")
-    assert abs(parser.parse("P(X==2)")-0.18394) < 0.0001
+    parser.Parser().parse("X~Po(1)")
+    assert abs(parser.Parser().parse("P(X==2)")-0.18394) < 0.0001
 
 def test_prob_po_less_eq():
     main.defined_vars = {}
-    parser.parse("X~Po(1)")
-    assert abs(parser.parse("P(X<=2)")-0.91969) < 0.0001
+    parser.Parser().parse("X~Po(1)")
+    assert abs(parser.Parser().parse("P(X<=2)")-0.91969) < 0.0001
