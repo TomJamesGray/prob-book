fe70e66252a07321ba706fb5cd9fb57f77ff2b77
Tom Gray <tomg77@yahoo.co.uk>
2019-09-17 17:23:59 +0100

    Added setup.py and file to install the kernelspec

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="prob-book",
+    version="0.1",
+    scripts=["prob-book"],
+    author="Tom Gray",
+    description="Interactive terminal and jupyter notebook kernel for probability calculations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TomJamesGray/prob-book",
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
