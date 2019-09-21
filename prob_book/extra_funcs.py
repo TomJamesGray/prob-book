d1878c3639cba9763906c7c7443d38c9194e5774
Tom Gray <tomg77@yahoo.co.uk>
2019-09-21 18:28:35 +0100

    Actually made min work like in the docs

--- prob_book/extra_funcs.py
+++ prob_book/extra_funcs.py
@@ -50,4 +50,19 @@ def max_f(*args):
     return x
 
 def min_f(*args):
-    pass
+    if type(args[0]) in (float,int):
+        x = args[0]
+    else:
+        x = min(args[0])
+
+    for val in args:
+        if type(val) in (float, int):
+            if val < x:
+                x = val
+        elif hasattr(val, "__iter__"):
+            if min(val) < x:
+                x = min(val)
+        else:
+            raise ValueError("Value {}, type {}, isn't numeric or iterable".format(val,type(val)))
+
+    return x
