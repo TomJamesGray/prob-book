eba040079e11aaa6be9759d50492859919fccc5c
Tom Gray <tomg77@yahoo.co.uk>
2019-09-22 15:22:47 +0100

    Changed probability equality back to "="

--- prob_book/tests/test_prob.py
+++ prob_book/tests/test_prob.py
@@ -4,7 +4,7 @@ from prob_book.parsing import parser
 def test_prob_po_eq():
     main.defined_vars = {}
     parser.Parser().parse("X~Po(1)")
-    assert abs(parser.Parser().parse("P(X==2)")-0.18394) < 0.0001
+    assert abs(parser.Parser().parse("P(X=2)")-0.18394) < 0.0001
 
 def test_prob_po_less_eq():
     main.defined_vars = {}
