3ee7cc07efc3bf74e3a3596edcaacb07cf246253
Tom Gray <tomg77@yahoo.co.uk>
2019-09-23 18:05:39 +0100

    Improved the error handling somewhat

--- prob_book/parsing/parser.py
+++ prob_book/parsing/parser.py
@@ -2,6 +2,7 @@ import os
 import inspect
 from lark import Lark
 from prob_book.parsing import evaluation
+from prob_book.exceptions import MismatchedBrackets
 
 # Set the client type to terminal by default, this affects how the plotting functions
 # displays the plot
@@ -13,6 +14,10 @@ class Parser:
         self.parser = Lark.open(os.path.join(os.path.dirname(inspect.stack()[0][1]), "grammar.lark"))
 
     def parse(self,line):
+        if line.count("(") != line.count(")"):
+            raise MismatchedBrackets
+
         tree = self.parser.parse(line)
+
         return self.eval.transform(tree)
 
